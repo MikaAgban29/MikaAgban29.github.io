dd88d80e0d2d0c26f74179ecc1ae3278a9664f46
Mika <mm.agban@gmail.com>
2026-01-07 21:42:04 +0800

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Scroll reveal animation
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  const revealPoint = 100;
+
+  reveals.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight - revealPoint) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
